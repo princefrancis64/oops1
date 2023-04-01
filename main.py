6a0e591a1089566999edc8af97679bfcf59406f0
Prince Francis <prince.francis64@gmail.com>
2023-04-02 02:58:16 +0530

    commit 3

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -12,6 +7,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    print("bye world")
-
+    print("Hello world")
+    print("Hello World")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
